c4a8ce29dc912b349e941f54603a2d1d4bdfe661
sameer bhardwaj <bhardwajsameer7@gmail.com>
2024-10-06 16:28:19 +0100

    Fixed CORS issue, allow only react js application deployed on aws amplify

--- src/main/java/com/grex/security/SecurityConfiguration.java
+++ src/main/java/com/grex/security/SecurityConfiguration.java
@@ -48,7 +48,7 @@ public class SecurityConfiguration {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("https://main.d19sra56k1kvne.amplifyapp.com"));
         configuration.setAllowedMethods(List.of("GET","POST"));
         configuration.setAllowedHeaders(List.of("Authorization","Content-Type"));
         configuration.setAllowCredentials(true);
