4331fe7f032fcc205c6dedd673687b0bea35c755
sameer bhardwaj <bhardwajsameer7@gmail.com>
2024-10-27 23:37:50 +0000

    Added Bot Service

--- src/main/java/com/grex/persistence/BotRepository.java
+++ src/main/java/com/grex/persistence/BotRepository.java
@@ -4,12 +4,9 @@ import com.grex.model.User;
 import org.apache.ibatis.annotations.*;
 
 @Mapper
-public interface UserAuthenticationRepository {
+public interface BotRepository {
 
-    @Select("SELECT count(*) > 0 FROM GREX_USER WHERE user_email = #{user_email} OR user_stage_name = #{user_stage_name} ")
-    boolean checkEmailOrStageNameAlreadyExists(@Param("user_email") String user_email, @Param("user_stage_name") String user_stage_name);
-
-    @Select("SELECT * FROM GREX_USER WHERE user_email = #{user_email}")
+    @Select("SELECT * FROM GREX_USER  WHERE user_is_bot= true ORDER BY RANDOM() LIMIT 1")
     @Results({
             @Result(property = "username", column = "user_email"),
             @Result(property = "stageName", column = "user_stage_name"),
@@ -18,12 +15,5 @@ public interface UserAuthenticationRepository {
             @Result(property = "isActive", column = "user_is_active"),
             @Result(property = "isBot", column = "user_is_bot")
     })
-    public User findByEmail(@Param("user_email") String user_email);
-
-    @Insert("INSERT INTO GREX_USER VALUES( #{user_email}, #{user_stage_name}, #{user_password}, #{user_role}, #{user_is_active},#{user_is_bot} )")
-    int addNewUser(@Param("user_email") String user_email, @Param("user_stage_name") String user_stage_name, @Param("user_password") String user_password, @Param("user_role") String user_role, @Param("user_is_active") boolean user_is_active,@Param("user_is_bot") boolean user_is_bot );
-
-
- /*   @Delete("DELETE FROM GREX_USER_OTP WHERE user_email = #{user_email}")
-    void deleteOtpRecord(@Param("user_email") String user_email);*/
+    public User getRandomBotUser();
 }
--- src/main/java/com/grex/service/BotService.java
+++ src/main/java/com/grex/service/BotService.java
@@ -1,2 +1,57 @@
-package com.grex.service;public class BotService {
+package com.grex.service;
+
+import com.grex.model.Progress;
+import com.grex.model.User;
+import com.grex.persistence.BotRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Random;
+
+@Service
+public class BotService {
+
+    private final BotRepository botRepository;
+    private final CacheService cacheService;
+
+    private final ProgressService progressService;
+
+    @Autowired
+    public BotService(BotRepository botRepository, CacheService cacheService, ProgressService progressService) {
+        this.botRepository = botRepository;
+        this.cacheService = cacheService;
+        this.progressService = progressService;
+    }
+
+    private static final Logger logger = LoggerFactory.getLogger(BotService.class);
+
+    @Scheduled(fixedRate = 60000)  // every minute
+    @Transactional
+    public void runBot() {
+
+        logger.info("starting bots");
+
+        // get a random bot user from grex_user table
+        final String  bot_stage_name =  botRepository.getRandomBotUser().getStageName().trim();
+
+        //load progress from table and put in cache
+        Progress progress = progressService.findAllGroupProgressByStageName(bot_stage_name);
+        cacheService.getProgressMap().put(bot_stage_name,progress);
+
+       //update random column by 1 for random bot
+        cacheService.setCachedProgressByGroup(bot_stage_name,getRandomGroupColumn());
+
+        logger.info("exit bots");
+
+    }
+
+    private String getRandomGroupColumn() {
+        Random random = new Random();
+        int randomNumber = random.nextInt(200) + 1; // Generates a number between 1 and 200
+        return "g" + randomNumber;
+    }
 }
