0b73cea66baada47c5160d7f20222267042c2119
sameer bhardwaj <bhardwajsameer7@gmail.com>
2024-11-03 19:06:19 +0000

    Adding Group , Words & FlashCard in bunny.net cdn caching based on URL

--- src/main/java/com/grex/controller/GroupController.java
+++ src/main/java/com/grex/controller/GroupController.java
@@ -4,8 +4,11 @@ import com.grex.dto.FlashCardDto;
 import com.grex.dto.GenericMessage;
 import com.grex.dto.GroupDto;
 import com.grex.service.GroupService;
+import com.grex.service.UserAuthenticationService;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/grex")
+@RequestMapping("/api/grex/cdn")
 public class GroupController {
 
     private final GroupService groupService;
@@ -25,14 +28,20 @@ public class GroupController {
         this.groupService = groupService;
     }
 
+    private static final Logger logger = LoggerFactory.getLogger(GroupController.class);
 
     @GetMapping("/groups")
     public ResponseEntity<GenericMessage> getGroups() {
+
+        logger.info("getting groups");
+
         return new ResponseEntity<>(new GenericMessage(HttpStatus.OK, groupService.getGroupsFromRedis()), HttpStatus.OK);
     }
 
     @GetMapping("/groups/{groupId}")
     public ResponseEntity<GenericMessage> getGroupById(@PathVariable @NotNull @NotBlank String groupId) {
+
+        logger.info("getting groups by id"+groupId);
 
         GroupDto groupDto = groupService.getGroupFromRedis(groupId);
 
@@ -45,6 +54,8 @@ public class GroupController {
 
     @GetMapping("/flashcard/{groupId}")
     public ResponseEntity<GenericMessage> getFlashCardByGroupId(@PathVariable @NotNull @NotBlank String groupId) {
+
+        logger.info("getting flashcards by group id"+groupId);
 
         FlashCardDto flashCardDto = groupService.getFlashCardFromRedis(groupId);
 
